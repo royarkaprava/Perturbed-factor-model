a5c2ebad75f82837a801dc5f79d8fbce1d7aaef2
royarkaprava <34636927+royarkaprava@users.noreply.github.com>
2020-04-29 10:32:46 -0400

    Update LRFpertgrp1.R

--- LRFpertgrp1.R
+++ LRFpertgrp1.R
@@ -40,6 +40,7 @@ po <- 1
 Qmean <- array(1*diag(p))
 set.seed(1)
 
+#################Generating the perturbation matrices##########################
 for(i in 2:grp){
   Qlist[, i] <- array(matrix(rnorm(p^2, Qmean, sd = sqrt(0.0001)), p, p))
 }
