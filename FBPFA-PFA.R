3e9a13fcbdf79eb4df821f6a512125bf4e0bf4b8
royarkaprava <34636927+royarkaprava@users.noreply.github.com>
2020-04-29 09:55:51 -0400

    Update FBPFA-PFA.R

--- FBPFA-PFA.R
+++ FBPFA-PFA.R
@@ -3,11 +3,6 @@ library(MASS)
 library(pracma)
 library(expm)
 
-QYprG <- function(i, mat = Y, vec = grpind, Ql = Qlist){
-  p <- nrow(Y)
-  temp <- matrix(Ql[, vec[i]], p, p)
-  return(temp%*%mat[, i])
-}
 
 ################################################Function#################################################### 
 #' @title The function is to fit PFA, FBPFA and Common Factor model with heterscedastic latent factors
@@ -34,39 +29,6 @@ QYprG <- function(i, mat = Y, vec = grpind, Ql = Qlist){
 #'
 #' @export
 #' @examples
-#'set.seed(1)
-# data generation
-#'
-#' n <- 500
-#' p  <- 21
-#' r  <- 5
-#' grp <- 10
-#' 
-#' 
-#' eta0 <- matrix(rnorm(r*n), r, n) 
-#' 
-#' lambda0           <- matrix(0, p, r)
-#' lambda0[1:5, 1]   <- rnorm(5, mean = 5, sd = 1)
-#' lambda0[5:9, 2]   <- rnorm(5, mean = 5, sd = 1)
-#' lambda0[9:13, 3]  <- rnorm(5, mean = 5, sd = 1)
-#' lambda0[13:17, 4] <- rnorm(5, mean = 5, sd = 1)
-#' lambda0[17:21, 5] <- rnorm(5, mean = 5, sd = 1)
-#' 
-#' Y <- matrix(rnorm(p*n, mean = lambda0 %*% eta0, sd = 1), p, n)#matrix(rnorm(p*n, mean = lambda0 %*% eta0, sd = 1), p, n)
-#' Qlist <- matrix(array(diag(p)), p^2, grp)
-#' Qmean <- array(1*diag(p))
-#' 
-#' set.seed(100)
-#' Qlist <- matrix(array(diag(p)), p^2, grp)
-#' Qmean <- array(1*diag(p))
-#' set.seed(1)
-#' 
-#' for(i in 2:grp){
-#'   Qlist[, i] <- array(matrix(rnorm(p^2, Qmean, sd = sqrt(0.0001)), p, p))
-#' }
-#' grpind = rep(1:10, each = 50)
-#' Y <- parallel::mcmapply(1:n, FUN = QYprG, MoreArgs = list(mat=Y)) #matrix(Q %*% array(Y), p, n)
-#' fit <- PFA(Y, grpind = rep(1:10, each = 50))
 
 PFA <- function(Y=Y, d = 10, grpind = NULL, measureerror = F, FB=T, alph= 0.0001, Total_itr = 5000){
   QYpr <- function(i, mat = Y, vec = grpind, Ql = Qlist){
@@ -154,7 +116,7 @@ PFA <- function(Y=Y, d = 10, grpind = NULL, measureerror = F, FB=T, alph= 0.0001
   po <- 1
   Qmean <- array(1*diag(p))
   Q_p <- list()
-  alph <- 0.01
+  alph <- alph
   alph_p <- rep(0, Total_itr)
   Qiup <- function(i, alphf){
     Qtemp    <- matrix(Qlistmat[, i], p, p)
